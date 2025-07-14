6b7d6f2a741fa7c9e690d6bad607574cc9b25fc0
Bisrat Haile <bisrathaile1919@gmail.com>
2025-07-15 02:26:19 +0300

    �️ Create Dagster job for full pipeline (load, transform, enrich)

--- orchestration/jobs.py
+++ orchestration/jobs.py
@@ -1,10 +1,10 @@
 from dagster import job
-from ops.load_raw import load_raw_data
-from ops.yolo_detect import run_yolo_detection
-from ops.build_dbt_models import run_dbt_models
+from orchestration.ops.load_raw import load_raw_to_postgres_op
+from orchestration.ops.yolo_detect import yolo_detect_op
+from orchestration.ops.build_dbt_models import run_dbt_models_op
 
 @job
-def full_pipeline():
-    raw = load_raw_data()
-    detection = run_yolo_detection()
-    dbt_run = run_dbt_models()
+def telegram_pipeline():
+    load_raw_to_postgres_op()
+    run_dbt_models_op()
+    yolo_detect_op()
