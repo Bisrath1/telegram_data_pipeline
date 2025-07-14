45e7395236a40dac22536ceb357111c442ee8c7f
Bisrat Haile <bisrathaile1919@gmail.com>
2025-07-15 02:26:07 +0300

    �️ Add Dagster op to run dbt transformations

--- orchestration/ops/build_dbt_models.py
+++ orchestration/ops/build_dbt_models.py
@@ -1,7 +1,16 @@
 from dagster import op
 import subprocess
+import os
 
 @op
-def run_dbt_models():
-    print("Running dbt models...")
-    subprocess.run(["dbt", "run"], check=True)
+def run_dbt_models_op():
+    """
+    Runs `dbt run` to transform data in the warehouse using dbt models.
+    """
+    os.chdir("telegram_medical_dbt")
+    result = subprocess.run(["dbt", "run"], capture_output=True, text=True)
+
+    if result.returncode != 0:
+        raise Exception(f"DBT run failed:\n{result.stderr}")
+    else:
+        print(f"DBT Output:\n{result.stdout}")
